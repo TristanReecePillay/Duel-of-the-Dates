2aa83ce22af88402126f86d23152d70a0e6e0549
TristanReecePillay <tristan27sharks@gmail.com>
2023-10-13 13:43:10 +0200

    Base Game Complete

--- Duel of the Dates/Assets/Scripts/ScoreManager.cs
+++ Duel of the Dates/Assets/Scripts/ScoreManager.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
+    public int scoreToReach;
+
     private int player1score = 0;
     private int player2score = 0;
 
@@ -17,11 +20,21 @@ public class ScoreManager : MonoBehaviour
         player1score++;
         player1ScoreText.text = player1score.ToString();
         Debug.Log("Player 1 Score: " +  player1score);
+        CheckScore();
     }
     public void Player2Goal()
     {
         player2score++;
         player2ScoreText.text = player2score.ToString();
         Debug.Log("Player 2 Score: " + player2score);
+        CheckScore();
+    }
+
+    private void CheckScore()
+    {
+        if(player1score == scoreToReach)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
